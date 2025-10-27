8452e573833f674024fd73f35f6e786960156969
vieerr <olivier_paspuel@hotmail.com>
2025-10-27 15:57:36 -0500

    feat(config): 🎸 initial git cz config & flutter initialization

---
+++ android/app/src/main/kotlin/com/example/saint_roche_atomic_design/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.saint_roche_atomic_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ changelog.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  questions: ['type', 'scope', 'subject', 'body', 'breaking', 'issues', 'lerna'],
+  scopes: [
+    'config',
+    'home'
+  ],
+  maxMessageLength: 100,
+}
